95ba8675faf5dd850d2ed004a5f325a5cf678664
Jiří Vokřínek <jiri.vokrinek@tul.cz>
2023-04-23 18:24:02 +0000

    Initialization of semestral project

---
+++ server/src/main/java/cz/tul/fm/jiri_vokrinek/stin_semestral/Server.java
@@ -0,0 +1,11 @@
+package cz.tul.fm.jiri_vokrinek.stin_semestral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server {
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+    }
+}
